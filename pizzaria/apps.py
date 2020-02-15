c9ca8f32b15bee6ab93877801bb182a6af777b68
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-15 19:33:09 -0300

    chore: add django files to zip

--- pizzaria/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzariaConfig(AppConfig):
-    name = 'pizzaria'
--- pizzaria_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from pizzaria import urls
-
-urlpatterns = [
-    path('', include(urls)),
-    path('admin/', admin.site.urls),
-]
